bb3aa7618946ad83ff2617a884751190cf5f44a2
Federico Cecconi <ghiande-boe0a@icloud.com>
2023-09-25 11:01:19 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Federico Cecconi on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
